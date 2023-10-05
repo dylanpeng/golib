794e3c83e16b2a9ca6e5107c8e80f68ce562da97
peng.chen <chen_peng7@163.com>
2023-10-05 12:18:54 +0800

    20231005 add proto coder

---
+++ coder/proto.go
@@ -0,0 +1,60 @@
+package coder
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+)
+
+const (
+	EncodingProtobuf    = "protobuf"
+	ContentTypeProtobuf = "application/x-protobuf"
+)
+
+var ProtoCoder = &protoCoder{}
+
+type protoCoder struct{}
+
+func (c *protoCoder) Unmarshal(data []byte, v interface{}) error {
+	pb, ok := v.(proto.Message)
+
+	if !ok {
+		return errors.New("invalid protobuf message")
+	}
+
+	return proto.Unmarshal(data, pb)
+}
+
+func (c *protoCoder) Marshal(v interface{}) ([]byte, error) {
+	pb, ok := v.(proto.Message)
+
+	if !ok {
+		return nil, errors.New("invalid protobuf message")
+	}
+
+	return proto.Marshal(pb)
+}
+
+func (c *protoCoder) DecodeRequest(ctx *gin.Context, v interface{}) (err error) {
+	data, err := GetRequestBody(ctx)
+
+	if err != nil {
+		return
+	}
+
+	return c.Unmarshal(data, v)
+}
+
+func (c *protoCoder) SendResponse(ctx *gin.Context, v interface{}) (err error) {
+	ctx.Header(EncodingHeader, EncodingProtobuf)
+	ctx.Header(ContentTypeHeader, ContentTypeProtobuf)
+
+	data, err := c.Marshal(v)
+
+	if err != nil {
+		return
+	}
+
+	_, err = ctx.Writer.Write(data)
+	return
+}
